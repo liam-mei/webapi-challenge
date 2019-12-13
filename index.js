75993f16ea39d513a5ef3198fd251475302999f7
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-13 03:17:14 -0600

    finished index

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+require("dotenv").config();
 
-Sing along:
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
+const server = express();
 
-Go code!
-*/
+// You can list out all the middleware in a row or in separate lines
+server.use(express.json(), helmet(), morgan("dev"));
+
+server.get("/", (req, res) => {
+  res.json({ sanityCheck: "I work", envMessage: process.env.MESSAGE || "env not working/undefined" });
+});
+
+// Routers
+
+
+// global error handling
+server.use((req, res) => res.status(404).json({ error: "route not found" }));
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ errorMessage: "some error occurred, please try again later", err });
+});
+
+
+const port = process.env.PORT || 8080
+server.listen(port, () => {
+  console.log(`\n*** server listening on port: ${port} *** \n`)
+})
