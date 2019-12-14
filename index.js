dfeb79276787c9543c387edb186f8bfd7a8a81ee
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-14 01:56:47 -0600

    updated global error handler
    error handler now accepts status codes and error messages

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 
+const projectRouter = require("./routers/projectRouter");
+const actionRouter = require("./routers/actionRouter");
+
 const server = express();
 
 // You can list out all the middleware in a row or in separate lines
@@ -14,17 +17,20 @@ server.get("/", (req, res) => {
 });
 
 // Routers
-
+server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
 
 // global error handling
 server.use((req, res) => res.status(404).json({ error: "route not found" }));
 
 server.use((err, req, res, next) => {
-  res.status(500).json({ errorMessage: "some error occurred, please try again later", err });
+  const { statusCode, errorMessage, originalError } = err;
+  res
+    .status(statusCode || 500)
+    .json({ errorMessage: errorMessage || "some error occurred, please try again later", originalError });
 });
 
-
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
-  console.log(`\n*** server listening on port: ${port} *** \n`)
-})
+  console.log(`\n*** server listening on port: ${port} *** \n`);
+});
