755600248817c459762e1cc3282095c7f8ca4fbd
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-14 01:57:08 -0600

    projectMiddleware

--- middleware/projectMiddleware.js
+++ middleware/projectMiddleware.js
@@ -1,11 +1,30 @@
 const db = require("../data/helpers/projectModel");
 
 function validateProject(req, res, next) {
-  const { project_id, description, notes } = req.body;
+  const { name, description } = req.body;
 
-  const 
+  const errors = {};
+  if (!name) errors.missingName = "Please Enter Name";
+  if (!description) errors.missingDescription = "Please Enter Description";
+  if (Object.keys(errors).length) res.status(400).json(errors);
+
+  next();
 }
 
-function validateProjectId(req, res, next) {}
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  db.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        // res.status(400).json({ error: "Project of That Id does not exist" });
+        next({ statusCode: 400, errorMessage: "Project of that Id does not exist" });
+      }
+    })
+    .catch(err => next({ originalError: err }));
+}
 
 module.exports = { validateProject, validateProjectId };
